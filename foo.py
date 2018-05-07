4fe17741308a28e9f65bfee9a025dc72fdf6c267
Federico Lopez <fedelopez77@gmail.com>
2018-05-07 19:34:37 +0200

    Adding 'linker' to wikipedia

--- foo.py
+++ foo.py
@@ -1,6 +1,9 @@
 
+import wptools
+
 
 IGNORED_ENTITY_TYPES = {"ORDINAL", "NUMBER"}
+
 
 class Mention:
 
@@ -21,6 +24,36 @@ class Mention:
         self.end_line_number += 1
 
 
+class Entry:
+    """
+    It models the entry in a given knowledge base. For now it is just a wrapper for wptools.page.WPToolsPage
+    """
+
+    def __init__(self, page):
+        self.page = page
+
+
+class LinkedMention:
+
+    def __init__(self, mention, entry):
+        """
+        :param mention:
+        :param entry: Entry in a KB
+        """
+        self.mention = mention
+        self.entry = entry
+
+    def __str__(self):
+        if self.entry:
+            return "{} - {}".format(self.mention.string, self.entry.data["url"])
+        return "{} - NIL".format(self.mention.string)
+
+    def __repr__(self):
+        if self.entry:
+            return "{} - {}".format(self.mention.string, self.entry.data["url"])
+        return "{} - NIL".format(self.mention.string)
+
+
 def get_mentions_from_file(file_name):
     result = []
     i = 0
@@ -33,7 +66,6 @@ def get_mentions_from_file(file_name):
 
             line = line.split('\t')
             entity_type = line[4]
-            print("Line {}: '{}'".format(i, entity_type))
             if entity_type != 'O' and entity_type not in IGNORED_ENTITY_TYPES:
 
                 if entity_type == previous_type:
@@ -47,4 +79,14 @@ def get_mentions_from_file(file_name):
     return result
 
 
+def link_mentions(mentions):
+    result = []
+    for mention in mentions:
+        try:
+            page = wptools.page(mention.string).get_query()
+        except LookupError:
+            page = None
 
+        result.append(LinkedMention(mention, page))
+
+    return result
