89288f94d59072ff7db9c4871fbaf1aacb01412d
Federico Lopez <fedelopez77@gmail.com>
2018-06-04 17:54:33 +0200

    Converting dataset to TAC15 format

---
+++ dataset/gold_converter.py
@@ -0,0 +1,14 @@
+"""
+The purpose of this script is to convert the gold standards of the Wikifier ACE 2004 dataset to the TAC15 format
+in order to be able to calculate the metrics over this.
+
+Expected format:
+run_id  mention_id  mention_head    doc_id  kb_entry    entity_type mention_type(NAM)   confidence_value
+
+OJO! El entity type no lo tengo...
+
+VER TEMA DEL OFFSET:
+alcanza con Offset : Offset + length? (Creería que sí)
+"""
+
+
