b675384608115feb290728c2c2f7d40e52c7f603
Federico Lopez <fedelopez77@gmail.com>
2018-06-04 17:06:15 +0200

    Adding evaluation script

---
+++ evaluate.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Taken from https://github.com/wikilinks/neleval/blob/master/scripts/run_tac15_evaluation.sh
+
+set -e
+
+usage="Usage: $0 GOLD_FILE SYSTEMS_FILE OUT_DIR NUM_JOBS [-x EXCLUDED_SPANS]"
+
+if [ "$#" -lt 4 ]; then
+    echo $usage
+    exit 1
+fi
+
+cleanup_cmd='OFS="\t" {} $6 == "TTL/NAM" {$6 = "PER/NOM"} ($6 ~ /NAM$/ || ($6 ~ /PER.NOM$/ && $1 ~ /^ENG/)) && $1 != "CMN_NW_001331_20150702_F00100023"'
+
+gtab=$1; shift # gold standard link annotations (tab-separated)
+sysfile=$1; shift # file containing output from systems
+outdir=$1; shift # directory to which results are written
+jobs=$1; shift # number of jobs for parallel mode
+
+SCR=`dirname $0`
+
+
+# CONVERT GOLD TO EVALUATION FORMAT
+echo "INFO Converting gold to evaluation format.."
+# XXX: "combined" is a misnomer in tac15. Should be neleval? But existing scripts depend on this extension.
+gold=$outdir/gold.combined.tsv
+options=$@
+python -m neleval prepare-tac15 $gtab $options | awk -F'\t' "$cleanup_cmd" > $gold
+
+# convert systems to evaluation format
+echo "INFO converting systems to evaluation format.."
+my_results=$outdir/$sysfile.combined.tsv
+python -m neleval prepare-tac15 $sysfile $options | awk -F'\t' "$cleanup_cmd" > $my_results
+
+# EVALUATE
+echo "INFO Evaluating systems.."
+python -m neleval evaluate -m all -f tab -g $gold $my_results > $outdir/results.evaluation
